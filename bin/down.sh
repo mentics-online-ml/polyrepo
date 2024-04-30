ab7fd4dd3dff4e328898b1dddd0aa98a52ba0e0c
Joel Shellman <joel@mentics.com>
2024-04-30 10:45:54 -0700

    fix license and new versions

--- bin/down.sh
+++ bin/down.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# git submodule update --recursive --remote
-
 . "${BASH_SOURCE%/*}/common.sh"
 
 submodule_foreach_parallel "git pull"
--- bin/orig-setup.sh
+++ bin/orig-setup.sh
@@ -6,7 +6,7 @@ script=$(basename "$0")
 mapfile -t NAMES < repo.list
 cmds=()
 for NAME in "${NAMES[@]}"; do
-    cmds+=("git submodule add git@github.com:mentics-ml-demo/'${NAME}'.git '${NAME}'")
+    cmds+=("git submodule add git@github.com:mentics-online-ml/'${NAME}'.git '${NAME}'")
 done
 
 run_parallel "${script}" "${cmds[@]}"
